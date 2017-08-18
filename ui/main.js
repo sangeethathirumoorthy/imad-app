38cb34c5487c461132a84fc4cf979c0b0b17f971
sangeethathirumoorthy <31065763+sangeethathirumoorthy@users.noreply.github.com>
2017-08-18 18:01:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 //var changes
 var element=document.getElementById("main-text");
 element.innerHTML="NEW VALUE"
@@ -14,4 +14,12 @@ function moveRight(){
 
 img.onclick=function(){
    var interval=setInterval(moveRight,50);
-};
+};*/
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function()
+{
+    counter=counter+1;
+    var span=document.getElementById('counter');
+    span.innerHTML=counter.toString();
+}
