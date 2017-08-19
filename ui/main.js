df22d556e9a9558ac1324e6d87044c10d5be95f1
sangeethathirumoorthy <31065763+sangeethathirumoorthy@users.noreply.github.com>
2017-08-19 14:33:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,11 +15,35 @@ function moveRight(){
 img.onclick=function(){
    var interval=setInterval(moveRight,50);
 };*/
-var button=document.getElementById("counter");
+//programtoincrement the counter using the span
+/*var button=document.getElementById("counter");
 var counter=0;
 button.onclick=function()
 {
     counter=counter+1;
     var span=document.getElementById("count");
     span.innerHTML=counter.toString();
-}
+}*/
+var button=document.getElementById("counter");
+button.onclick=function()
+{  //create a request to the counter endpoint
+    var request=new XMLHttpRequest();
+    
+   //capture the response and store it in a variable
+   request.onreadystatechange=function()
+   {
+       if(request.readystate===XMLHttpRequest.DONE)
+       {
+           //TAKE SOME ACTION
+           if(request.status==200)
+           {
+               var counter=request.responseText;
+               var span=document.getElementById("count");
+               span.innerHTML=counter.toString();
+           }
+       }
+    };
+ request.open('GET','http://tsangeetha.imad.hasura-app.io/counter',true);
+ request.send(null);
+   
+   };
