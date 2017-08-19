765090c90972e797a146a1b98d180b74562d721f
sangeethathirumoorthy <31065763+sangeethathirumoorthy@users.noreply.github.com>
2017-08-19 15:15:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ button.onclick=function()
            }
        }
     };
- request.open('GET','http://tsangeetha.imad.hasura-app.io',true);
+ request.open('GET','http://tsangeetha.imad.hasura-app.io/counter',true);
  request.send(null);
    
    };
